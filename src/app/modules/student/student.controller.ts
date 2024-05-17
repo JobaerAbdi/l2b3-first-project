74590bc133c883f1b052bb75a1634a289c206923
JobaerAbdi <jobaerrahman757@gmail.com>
2024-05-17 22:52:47 +0600

    router controller service complete

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express'
+import { StudentServices } from './student.service'
+
+const createStudentIntoDB = async (req: Request, res: Response) => {
+  try {
+    const { student } = req.body
+    const result = await StudentServices.createStudentIntoDB(student)
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const getAllStudents = async(req: Request, res: Response)=>{
+  try {
+    const result = await StudentServices.getAllStudentsFromDB()
+    res.status(200).json({
+      success: true,
+      message: 'Students are retrieved successfully',
+      data: result,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const getSingleStudent = async(req: Request, res: Response)=>{
+  try {
+    const {studentId} = req.params
+    const result = await StudentServices.getSingleStudentFromDB(studentId)
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const StudentControllers = {
+  createStudentIntoDB,
+  getAllStudents,
+  getSingleStudent
+}
---
+++ src/app/modules/student/student.service.ts
@@ -0,0 +1,22 @@
+import { TStudent } from "./student.interface";
+import { Student } from "./student.model";
+
+const createStudentIntoDB =async(payload:TStudent )=>{
+   const data = await Student.create(payload)
+   return data
+}
+
+const getAllStudentsFromDB = async()=>{
+    const result = await Student.find();
+    return result;
+}
+const getSingleStudentFromDB = async(id: string)=>{
+    const result = await Student.findOne({id});
+    return result;
+}
+
+export const StudentServices = {
+    createStudentIntoDB,
+    getAllStudentsFromDB,
+    getSingleStudentFromDB
+}
