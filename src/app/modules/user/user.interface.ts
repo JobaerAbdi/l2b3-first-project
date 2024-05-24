ef9e069863c4cb78a51b408af41f1d3af98fbeb7
JobaerAbdi <jobaerrahman757@gmail.com>
2024-05-24 12:59:46 +0600

    User schema and model created

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,10 @@
+type TUser = {
+    id: string;
+    password: string;
+    needsPasswordChange: boolean;
+    role: "admin" | "faculty" | "student";
+    status: "in-progress" | "blocked";
+    isDeleted: boolean;
+};
+
+export default TUser;
