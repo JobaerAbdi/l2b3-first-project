6298c9baf4ea3cecd7a40c0f4968c880ddd93ec1
JobaerAbdi <jobaerrahman757@gmail.com>
2024-05-30 21:44:04 +0600

    academecSemester validation create complete

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -1,39 +1,30 @@
 import { z } from 'zod'
 
+const semesterName = ['Autumn', 'Summer', 'Fall']
+const semesterCode = ['01', '02', '03']
+const semesterMonth = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
 const createAcademicSemesterSchema = z.object({
   body: z.object({
     academicSemesterData: z.object({
-      name: z.enum(['Autumn', 'Summer', 'Fall']),
+      name: z.enum([...semesterName] as [string, ...string[]]),
       year: z.string(),
-      code: z.enum(['01', '02', '03']),
-      startMonth: z.enum([
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ]),
-      endMonth: z.enum([
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ]),
+      code: z.enum([...semesterCode] as [string, ...string[]]),
+      startMonth: z.enum([...semesterMonth] as [string, ...string[]]),
+      endMonth: z.enum([...semesterMonth] as [string, ...string[]]),
     }),
   }),
 })
